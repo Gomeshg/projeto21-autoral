1bd5d6f2ad5abc82a1ed608804ba1d4acbca28c5
Hugo Gomes <gomes.hugo00@gmail.com>
2023-04-26 16:48:56 -0300

    feat: create many users and sessions in user factory. add functions in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,23 @@
-import { createUser, loginUser } from "./factories/user";
+import { createManyUsers, loginManyUsers } from "./factories/user-factory";
+import { createLine } from "./factories/line-factory";
+import prisma from "../src/database/prisma";
+import { User, Session } from "../src/protocols/contracts";
+import { Line, NewLine } from "../src/protocols/contracts";
+
+export async function seed() {
+  await prisma.line.deleteMany({});
+  await prisma.session.deleteMany({});
+  await prisma.user.deleteMany({});
+
+  const users: User[] = await createManyUsers();
+  const sessions: Session[] = await loginManyUsers(users);
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
